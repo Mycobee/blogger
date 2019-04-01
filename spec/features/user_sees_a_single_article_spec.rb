5939e85b3ed0959ebdd05ba7e5ab12c6a29eb77f
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-04-01 08:43:15 -0600

    finished comment functionality

--- spec/features/user_sees_a_single_article_spec.rb
+++ spec/features/user_sees_a_single_article_spec.rb
@@ -16,6 +16,7 @@ describe "user sees one article" do
             expect(page).to have_content(comment_2.body)
             expect(page).to have_content(article_1.title)
             expect(page).to have_content(article_1.body)            
+            expect(page).to have_content("Comments (2)")            
         end
     end
 
